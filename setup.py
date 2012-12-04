b22e32c29cff740239db0c881ef85428b3370fe0
Erin Sheldon <erin.sheldon@gmail.com>
2012-12-04 09:19:17 -0800

    scripts to install

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+import os
 import glob
 from distutils.core import setup
 
-scripts=glob.glob('desdb/bin/*.py')
-scripts += ['desdb/bin/des-query']
+scripts= ['des-query',
+          'get-coadd-info',
+          'get-coadd-srclists',
+          'get-filelist',
+          'get-red-info',
+          'get-release-runs',
+          'get-table-info',
+          'wget-des',
+          'wget-des-parallel',
+          'des-query']
+
+scripts=[os.path.join('desdb','bin',s) for s in scripts]
 
 setup(name="desdb", 
       version="0.1.0",
