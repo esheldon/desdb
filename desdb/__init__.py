649d52a7da6311311152ebda6011a61967c7e111
Erin Sheldon <erin.sheldon@gmail.com>
2013-12-10 23:36:24 -0500

    use relative imports

--- desdb/__init__.py
+++ desdb/__init__.py
@@ -8,12 +8,14 @@ from .files import DESFiles
 try:
     from . import desdb
 
-    from desdb import connect
-    from desdb import Connection
-    from desdb import CursorWriter 
-    from desdb import ObjWriter
-    from desdb import print_cursor
-    from desdb import cursor2dictlist
+    from .desdb import connect
+    from .desdb import Connection
+    from .desdb import CursorWriter 
+    from .desdb import ObjWriter
+    from .desdb import print_cursor
+    from .desdb import cursor2dictlist
+
+    from .desdb import array2table
 except:
     pass
 
